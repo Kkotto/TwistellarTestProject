6440506e21f4d4dcb1dd34eceb331bc9dc7df677
kkotto <ksen.sharupich@gmail.com>
2023-12-27 17:56:16 +0300

    added: retrieving data

--- force-app/main/default/lwc/serviceCaseQueueFiltered/serviceCaseQueueFiltered.js
+++ force-app/main/default/lwc/serviceCaseQueueFiltered/serviceCaseQueueFiltered.js
@@ -1,6 +1,12 @@
-import { LightningElement } from "lwc";
+import { LightningElement, wire } from "lwc";
 import Id from "@salesforce/user/Id";
+import getUserCases from "@salesforce/apex/ServiceCaseQueueService.getUserCases";
 
 export default class ServiceCaseQueueFiltered extends LightningElement {
+  // Get id of current user
   userId = Id;
+
+  // Get all cases owned by current user
+  @wire(getUserCases, { userId: "$userId" })
+  caseList = [];
 }
