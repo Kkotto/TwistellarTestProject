041032410d3ad6926aa8884786d64e287e0d0bbd
kkotto <ksen.sharupich@gmail.com>
2023-12-27 16:20:56 +0300

    added: serviceCaseQueueFiltered lwc

--- force-app/main/default/lwc/serviceCaseQueueFiltered/serviceCaseQueueFiltered.js
+++ force-app/main/default/lwc/serviceCaseQueueFiltered/serviceCaseQueueFiltered.js
@@ -1,3 +1,6 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
+import Id from "@salesforce/user/Id";
 
-export default class ServiceCaseQueueFiltered extends LightningElement {}
+export default class ServiceCaseQueueFiltered extends LightningElement {
+  userId = Id;
+}
