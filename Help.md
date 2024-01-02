d4860ddc359a8ffbf2a0c441e40dfb1df49ea706
kkotto <ksen.sharupich@gmail.com>
2024-01-02 15:53:13 +0300

    updated: open case record page

--- Help.md
+++ Help.md
@@ -5,3 +5,5 @@
 // Field formulas: https://help.salesforce.com/s/articleView?id=000386076&type=1
 // Sorting field formulas: https://www.salesforceben.com/everything-you-need-to-know-about-salesforce-queues/
 // Sorting by picklists values: https://www.salesforceben.com/salesforce-case-scoring/
+// Navigation Mixin: https://www.sfdcpoint.com/salesforce/navigation-service-in-lwc/
+// Dataset id: https://stackoverflow.com/questions/49711130/i-used-e-currenttarget-dataset-id-to-get-the-inex-of-the-clicked-li-but-gives
--- force-app/main/default/lwc/serviceCaseQueueFiltered/serviceCaseQueueFiltered.js
+++ force-app/main/default/lwc/serviceCaseQueueFiltered/serviceCaseQueueFiltered.js
@@ -1,12 +1,27 @@
 import { LightningElement, wire } from "lwc";
+import { NavigationMixin } from "lightning/navigation";
 import Id from "@salesforce/user/Id";
 import getUserCases from "@salesforce/apex/ServiceCaseQueueService.getUserCases";
 
-export default class ServiceCaseQueueFiltered extends LightningElement {
+export default class ServiceCaseQueueFiltered extends NavigationMixin(
+  LightningElement
+) {
   // Get id of current user
   userId = Id;
 
   // Get all cases owned by current user
   @wire(getUserCases, { userId: "$userId" })
   caseList = [];
+
+  openCaseRecord(event) {
+    const caseId = event.currentTarget.dataset.id;
+    console.log(caseId);
+    this[NavigationMixin.Navigate]({
+      type: "standard__recordPage",
+      attributes: {
+        recordId: caseId,
+        actionName: "view"
+      }
+    });
+  }
 }
